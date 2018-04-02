61132621d3db4e49b9bcc02ea105b573ea2b20c9
Martin Ghosal <martin_ghosal@hotmail.co.uk>
2018-04-02 12:59:56 +0100

    Fix camera by running the sensorservice on device and adding property camera.fifo.disable=1. Disabling the FakeSensorService in droidmedia is also required

--- device.mk
+++ device.mk
@@ -411,7 +411,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.enable_timeout_ms=12000
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.bt.bdaddr_path=/persist/bdaddr.txt
+    ro.bt.bdaddr_path=/persist/bdaddr.txt \
+    persist.camera.shutter.disable=1 \
+    camera.fifo.disable=1
 
 # limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
